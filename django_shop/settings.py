ee4fc5f8b0edbbb2f2ac0e27d3aba072f55c67a4
5ergey <akifyev.sergey@gmail.com>
2025-12-12 10:19:21 +0300

    add apps user and shop

--- django_shop/settings.py
+++ django_shop/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'user',
+    # ------ Приложения ------
     'shop',
+    'user',
 ]
 
 MIDDLEWARE = [
