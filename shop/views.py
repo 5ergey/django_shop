1c180dc1260f38164acfa55c8896a4d4b18080ad
5ergey <akifyev.sergey@gmail.com>
2025-12-24 15:02:22 +0300

    Переделана фильтрация, поиск и сортировка при помощи формы (вместо ajax) Добавлена пагинация

--- shop/views.py
+++ shop/views.py
@@ -8,6 +8,7 @@ class HomeView(generic.ListView):
     template_name = 'home.html'
     paginate_by = 3
 
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['categories'] = Category.objects.all()
